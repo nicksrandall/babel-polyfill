74b3f4c1991b7767e08a92cdec8f0be57e1fa21f
Esteban Arango Medina <marranoparael31@gmail.com>
2015-08-26 13:46:52 -0500

    Fallback for IE11 buggy Object.getOwnPropertyNames with iframe and window

--- browser-polyfill.js
+++ browser-polyfill.js
@@ -3296,5 +3296,31 @@
                 }(typeof global === "object" ? global : typeof window === "object" ? window : typeof self === "object" ? self : this)
             }).call(this, typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
         }, {}
+    ],
+    74:[
+        function(require, module, exports) {
+        		// fallback for IE11 buggy Object.getOwnPropertyNames with iframe and window
+            var $ = require("./$"),
+                toString = {}.toString,
+                getNames = $.getNames;
+
+						var windowNames = typeof window == 'object' && Object.getOwnPropertyNames
+						  ? Object.getOwnPropertyNames(window) : [];
+
+						function getWindowNames(it){
+						  try {
+						    return getNames(it);
+						  } catch(e){
+						    return windowNames.slice();
+						  }
+						}
+
+						module.exports = function getOwnPropertyNames(it){
+						  if(windowNames && toString.call(it) == '[object Window]')return getWindowNames(it);
+						  return getNames($.toObject(it));
+						};
+        }, {
+            "./$": 16
+        }
     ]
 }, {}, [1]);
